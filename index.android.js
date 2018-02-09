47bfff30132f509a59fb81acaf440e161fed91a1
Rodrigo <rodrigodot@outlook.com>
2018-02-09 12:57:09 -0200

    Hello World

--- index.android.js
+++ index.android.js
@@ -1,53 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
+  View,
   Text,
-  View
+  AppRegistry
 } from 'react-native';
 
-export default class app2 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const App = () => {
+  return(
+    <Text>Ola</Text>
+  );
+};
 
-AppRegistry.registerComponent('app2', () => app2);
+AppRegistry.registerComponent('app2', () => App);
