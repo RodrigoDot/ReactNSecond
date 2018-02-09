4e018e1a6901c649e228cbfdb4dea4f7c07b6ce1
Rodrigo <rodrigodot@outlook.com>
2018-02-09 16:12:03 -0200

    Usando um botao para disparar um evento

--- index.android.js
+++ index.android.js
@@ -2,40 +2,32 @@ import React from 'react';
 import {
   View,
   Text,
+  Button,
   AppRegistry
 } from 'react-native';
 
 const Estilos = {
-  container: {
-    flex: 1,
-    backgroundColor: '#7cb0f6',
-  },
-  header: {
-    flex: 2,
-    fontSize: 40,
-    backgroundColor: '#2932a1',
-  },
-  body: {
-    flex: 8,
-    fontSize: 40,
-    backgroundColor: '#424899',
-  },
-  footer: {
-    flex: 1,
-    fontSize: 40,
-    backgroundColor: '#a6a9d9',
+  principal: {
+    paddingTop: 40
   }
 };
 
+const onButtonCLick = () => {
+  alert('Clicked');
+}
+
 const App = () => {
 
-  const { header, body, footer, container } = Estilos;
+  const { principal } = Estilos;
 
   return(
-    <View style={ container }>
-      <Text style={ header }>Header</Text>
-      <Text style={ body }>Body</Text>
-      <Text style={ footer }>Footer</Text>
+    <View style={ principal }>
+      <Button
+        title= 'Click'
+        color= '#4a691a'
+        onPress= {onButtonCLick}
+        acessibilityLabel= 'Click aqui para visulizar'
+      />
     </View>
   );
 };
