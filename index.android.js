e5014b7c4df993fac7519578fe88a17f5cd14ce3
Rodrigo <rodrigodot@outlook.com>
2018-02-14 00:22:07 -0200

    Update app2 finisehd
    Added ALERT
    Added BUTTON
    Added TOUCHABLEOPACITY
    Added VIEW
    Added IMAGE
    Added ARRAY
    Added OBJECT

--- index.android.js
+++ index.android.js
@@ -3,29 +3,103 @@ import {
   View,
   Image,
   Text,
+  TouchableOpacity,
+  Alert,
   AppRegistry
 } from 'react-native';
 
-const Estilos = {
-  principal: {
-    paddingTop: 40
+const Styles = {
+  main: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   image: {
-    justifyContent: 'center'
+
+  },
+  bottomtButton: {
+    backgroundColor: '#26a327',
+    padding: 15,
+    paddingHorizontal: 85,
+    marginTop: 20
+  },
+  bottomButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
+};
+
+const onButtonClicked = () => {
+  var randomNumber = Math.floor(Math.random() * 7);
+
+  var texts = [
+    {
+      index : 0,
+      value: 'Hoje parece ser um belo dia para dormir.'
+    },
+    {
+      index : 1,
+      value: 'Adeus, e obrigado pelos peixes.'
+    },
+    {
+      index : 2,
+      value: 'Foi ótimo fingir estar sóbrio.'
+    },
+    {
+      index : 3,
+      value: 'Don`t Panic !!!'
+    },
+    {
+      index : 4,
+      value: 'É preciso rituais para que a vida faça sentido !'
+    },
+    {
+      index : 5,
+      value: 'Se a Lua não está no céu, será que ela arrumou um outro amor ?'
+    },
+    {
+      index : 6,
+      value: 'Se não foi o BIG-BANG, logo Deus é que não foi !'
+    },
+    {
+      index : 7,
+      value: 'Aconteça o que acontecer, não faz sentido esperar que seja diferente !'
+    }
+  ];
+
+  //poderia ser feito com o uso de um array da seguinte forma
+
+  // var texts = Array();
+  //
+  // texts[0] = 'frase';
+  // texts[1] = 'frase';
+  // texts[2] = 'frase';
+  // texts[3] = 'frase';
+  // texts[4] = 'frase';
+  // texts[5] = 'frase';
+
+  // Alert.alert(texts[randomNumber]);
+  // ...
+
+  Alert.alert(texts[randomNumber].value);
 };
 
 const App = () => {
 
-  const { principal, image } = Estilos;
+  const { main, image, bottomtButton, bottomButtonText } = Styles;
 
   return(
-    <View style={ principal }>
-      <Image style={ image }
-        source={ require('./imgs/uvas.png') }
-      >
-        <Text>Legenda foto</Text>
+    <View style={ main }>
+      <Image style={ image } source={ require('./imgs/logo.png') } >
       </Image>
+      <TouchableOpacity
+        style={ bottomtButton }
+        onPress={ onButtonClicked }
+        >
+        <Text style={ bottomButtonText }>Nova frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
